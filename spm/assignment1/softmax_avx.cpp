f70fbc506ee60d91076721a78c0beacf6b6b63c7
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-03-11 18:57:42 +0100

    assignment

--- spm/assignment1/softmax_avx.cpp
+++ spm/assignment1/softmax_avx.cpp
@@ -6,7 +6,29 @@
 #include <hpc_helpers.hpp>
 #include <avx_mathfun.h>
 
-void softmax_avx(const float *input, float *output, size_t K) {}
+void softmax_avx(const float *input, float *output, size_t K)
+{
+	// Find the maximum to stabilize the computation of the exponential
+	float max_val = -std::numeric_limits<float>::infinity();
+	for (size_t i = 0; i < K; ++i)
+	{
+		max_val = std::max(max_val, input[i]);
+	}
+
+	// computes all exponentials with the shift of max_val and the total sum
+	float sum = 0.0f;
+	for (size_t i = 0; i < K; ++i)
+	{
+		output[i] = std::exp(input[i] - max_val);
+		sum += output[i];
+	}
+
+	// normalize by dividing for the total sum
+	for (size_t i = 0; i < K; ++i)
+	{
+		output[i] /= sum;
+	}
+}
 
 std::vector<float> generate_random_input(size_t K, float min = -1.0f, float max = 1.0f)
 {
