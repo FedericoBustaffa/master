cc7fb58911415e439d81bd0d7cd3a33f68d2545f
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-03-11 12:06:00 +0100

    format

--- spm/assignment1/softmax_avx.cpp
+++ spm/assignment1/softmax_avx.cpp
@@ -2,57 +2,61 @@
 #include <vector>
 #include <random>
 #include <algorithm>
-#include <limits>      
+#include <limits>
 #include <hpc_helpers.hpp>
 #include <avx_mathfun.h>
 
+void softmax_avx(const float *input, float *output, size_t K) {}
 
-void softmax_avx(const float *input, float *output, size_t K) {
-
-}
-
-std::vector<float> generate_random_input(size_t K, float min = -1.0f, float max = 1.0f) {
-    std::vector<float> input(K);
-    //std::random_device rd;
-    //std::mt19937 gen(rd());
+std::vector<float> generate_random_input(size_t K, float min = -1.0f, float max = 1.0f)
+{
+	std::vector<float> input(K);
+	// std::random_device rd;
+	// std::mt19937 gen(rd());
 	std::mt19937 gen(5489); // fixed seed for reproducible results
-    std::uniform_real_distribution<float> dis(min, max);
-    for (size_t i = 0; i < K; ++i) {
-        input[i] = dis(gen);
-    }
-    return input;
+	std::uniform_real_distribution<float> dis(min, max);
+	for (size_t i = 0; i < K; ++i)
+	{
+		input[i] = dis(gen);
+	}
+	return input;
 }
 
-void printResult(std::vector<float> &v, size_t K) {
-	for(size_t i=0; i<K; ++i) {
-		std::fprintf(stderr, "%f\n",v[i]);
+void printResult(std::vector<float> &v, size_t K)
+{
+	for (size_t i = 0; i < K; ++i)
+	{
+		std::fprintf(stderr, "%f\n", v[i]);
 	}
 }
 
-
-int main(int argc, char *argv[]) {
-	if (argc == 1) {
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
 		std::printf("use: %s K [1]\n", argv[0]);
-		return 0;		
+		return 0;
 	}
-	size_t K=0;
-	if (argc >= 2) {
+	size_t K = 0;
+	if (argc >= 2)
+	{
 		K = std::stol(argv[1]);
 	}
-	bool print=false;
-	if (argc == 3) {
-		print=true;
-	}	
-	std::vector<float> input=generate_random_input(K);
+	bool print = false;
+	if (argc == 3)
+	{
+		print = true;
+	}
+	std::vector<float> input = generate_random_input(K);
 	std::vector<float> output(K);
 
 	TIMERSTART(softime_avx);
 	softmax_avx(input.data(), output.data(), K);
 	TIMERSTOP(softime_avx);
-	
+
 	// print the results on the standard output
-	if (print) {
+	if (print)
+	{
 		printResult(output, K);
 	}
 }
-
