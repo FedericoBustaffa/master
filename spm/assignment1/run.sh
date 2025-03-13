a9cdcbd0145be6d6815b5192d4af8530fb3493be
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-03-14 00:57:05 +0100

    avx

--- spm/assignment1/run.sh
+++ spm/assignment1/run.sh
@@ -6,8 +6,8 @@ make -j 2>&1 | grep softmax_auto.cpp
 for j in {128,256,512,1024,2048,4096,8192,16384}; do
     for i in {0..49}; do
         ./softmax_plain.out $j 1 1>> plain_times_$j.txt 2>> plain_res_$j.txt
-        ./softmax_auto.out $j 1 1>> auto_times_$j.txt 2>> auto_res_$j.txt
-        # ./softmax_avx.out $j 1 1>> avx_times_$j.txt 2>> avx_res_$j.txt
+        # ./softmax_auto.out $j 1 1>> auto_times_$j.txt 2>> auto_res_$j.txt
+        ./softmax_avx.out $j 1 1>> avx_times_$j.txt 2>> avx_res_$j.txt
     done
 done
 
@@ -26,15 +26,15 @@ function compare_results() {
 }
 
 for j in {128,256,512,1024,2048,4096,8192,16384}; do
-    compare_results plain_res_$j.txt auto_res_$j.txt
-    # compare_results plain_res_$j.txt avx_res_$j.txt
+    # compare_results plain_res_$j.txt auto_res_$j.txt
+    compare_results plain_res_$j.txt avx_res_$j.txt
     # compare_results auto_res_$j.txt avx_res_$j.txt
     echo "---------------"
 done
 
 # aggregate time files
 python aggregate.py *_times_*.txt
-mv results/auto.csv "results/$1.csv"
+# mv results/auto.csv "results/$1.csv"
 
 # delete all sparse files
 rm *.txt
