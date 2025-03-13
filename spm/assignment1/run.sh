445c08154eb257fab57e572bbb7e3b17b0f9a604
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-03-14 00:15:53 +0100

    results

--- spm/assignment1/run.sh
+++ spm/assignment1/run.sh
@@ -7,7 +7,7 @@ for j in {128,256,512,1024,2048,4096,8192,16384}; do
     for i in {0..49}; do
         ./softmax_plain.out $j 1 1>> plain_times_$j.txt 2>> plain_res_$j.txt
         ./softmax_auto.out $j 1 1>> auto_times_$j.txt 2>> auto_res_$j.txt
-        ./softmax_avx.out $j 1 1>> avx_times_$j.txt 2>> avx_res_$j.txt
+        # ./softmax_avx.out $j 1 1>> avx_times_$j.txt 2>> avx_res_$j.txt
     done
 done
 
@@ -27,13 +27,14 @@ function compare_results() {
 
 for j in {128,256,512,1024,2048,4096,8192,16384}; do
     compare_results plain_res_$j.txt auto_res_$j.txt
-    compare_results plain_res_$j.txt avx_res_$j.txt
-    compare_results auto_res_$j.txt avx_res_$j.txt
+    # compare_results plain_res_$j.txt avx_res_$j.txt
+    # compare_results auto_res_$j.txt avx_res_$j.txt
     echo "---------------"
 done
 
 # aggregate time files
 python aggregate.py *_times_*.txt
+mv results/auto.csv "results/$1.csv"
 
 # delete all sparse files
 rm *.txt
