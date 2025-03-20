2d5a4dcce9860c4180d4794085517dd9863f7f5c
FedericoBustaffa <kiko.bustaffa@gmail.com>
2025-03-20 13:00:12 +0100

    birth exp growth

--- cmcs/discrete/plot.py
+++ cmcs/discrete/plot.py
@@ -1,21 +1,28 @@
+import argparse
 import json
 
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help="filename to save the figure")
+    args = parser.parse_args()
+
     file = open("results/simple_birth.json", "r")
     data = json.load(file)
     file.close()
 
-    plt.figure(figsize=(8, 5), dpi=100)
+    plt.figure(figsize=(8, 5), dpi=300)
     plt.title("Exponential Growth")
+    plt.xlabel("Time")
+    plt.ylabel("Density")
 
     for d in data:
         if (
             d["iterations"] == 20
             and d["lambda"] == 1
             and d["sigma"] == 1
-            and d["delta"] == 1
+            and d["delta"] == 2
         ):
             plt.plot(
                 d["time_units"],
@@ -23,8 +30,10 @@ if __name__ == "__main__":
                 "--x",
                 label=rf"$N_0$ = {d['initial_density']}",
             )
+            plt.xticks(d["time_units"], labels=d["time_units"])
 
     plt.grid()
     plt.legend()
     plt.tight_layout()
+    plt.savefig(f"plots/{args.filename}.svg")
     plt.show()
